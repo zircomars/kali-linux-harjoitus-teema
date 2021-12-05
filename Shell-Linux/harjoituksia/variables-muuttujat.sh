5e65c92b27d6c33cc3c9ae1962336977715cf99e
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-12-05 13:50:45 +0200

    Create variables-muuttujat.sh

---
+++ Shell-Linux/harjoituksia/variables-muuttujat.sh
@@ -0,0 +1,15 @@
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+#console/outputs:::::::
+# $ ./main.sh zxcv
+# File Name: ./main.sh
+# First Parameter : zxcv
+# Second Parameter : 
+# Quoted Values: zxcv
+# Quoted Values: zxcv
+# Total Number of Parameters : 1
