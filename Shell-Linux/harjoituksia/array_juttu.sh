0666f26fd7ed828e3382ca5994b936a3b3edc573
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-12-05 20:51:35 +0200

    Create array_juttu.sh

---
+++ Shell-Linux/harjoituksia/array_juttu.sh
@@ -0,0 +1,12 @@
+#array juttut
+echo "array juttu"
+
+NAME[0]="Asdf"
+NAME[1]="Qwerty"
+NAME[2]="Zxc"
+NAME[3]="vbnm"
+NAME[4]="Dave"
+
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+echo "Third Index: ${NAME[4]}"
