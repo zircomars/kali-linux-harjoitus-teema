d2e481af2314baa8376aa45299118574b5e5b09a
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-12-22 21:33:36 +0200

    MULTILINE-COMMENT - kommentti ja muu muistio

---
+++ Shell-Linux/harjoituksia/echo-command2.sh
@@ -0,0 +1,22 @@
+#tämä on kommenti, mutta sitä ei tulostu esille, mutta jos on piempi lauseita tai riviä & ja voidaan määrittää koska päättyy
+
+<< 'MULTILINE-COMMENT'
+    Everything inside the
+    HereDoc body is
+    a multiline comment
+MULTILINE-COMMENT
+
+echo "adsf"
+
+#tämä on kommenti 2
+<< 'MULTILINE-COMMENT'
+ adsf
+ zcxv
+ qwery
+ 21357
+MULTILINE-COMMENT
+
+echo "Name?":
+read nimi
+
+echo "Nimi on: $nimi"
