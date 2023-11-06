7394453187388c0c8d70cd389b30e77197c45f35
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-06 12:22:57 +0200

    Create whois.sh

---
+++ Kali linux/kaikkiHarjoitukset/whois.sh
@@ -0,0 +1,142 @@
+# whois - luettelo (kuka on...) & whois - command
+# nimi TCP-palvelulle, työkalulle ja tietokantatyypille. Whois-tietokannat sisältää nimipalvelimen, rekisterinpitäjän ja joissakin tapauksissa täydelliset yhteystiedot a.:sta verkkotunnus (domain name). Jokaisen rekisterinpitäjän on ylläpidettävä Whois-tietokantaa, joka sisältää kaikki yhteystiedot tietoja verkkotunnuksista, joita he isännöivät. Ylläpidetään keskitettyä rekisterin Whois-tietokantaa InterNIC24:n toimesta.
+
+# nämä tietokannat julkaistaan yleensä Whois-palvelimella TCP:n kautta portti 43 ja ne ovat käytettävissä whois-asiakasohjelmalla
+# https://en.wikipedia.org/wiki/WHOIS
+
+┌──(kali㉿kali)-[~]
+└─$ whois megacorpone.com
+   Domain Name: MEGACORPONE.COM
+   Registry Domain ID: 1775445745_DOMAIN_COM-VRSN
+   Registrar WHOIS Server: whois.gandi.net
+   Registrar URL: http://www.gandi.net
+   Updated Date: 2023-06-13T18:08:24Z
+   Creation Date: 2013-01-22T23:01:00Z
+   Registry Expiry Date: 2024-01-22T23:01:00Z
+   Registrar: Gandi SAS
+   Registrar IANA ID: 81
+   Registrar Abuse Contact Email: abuse@support.gandi.net
+   Registrar Abuse Contact Phone: +33.170377661
+   Domain Status: clientTransferProhibited https://icann.org/epp#clientTransferProhibited
+   Name Server: NS1.MEGACORPONE.COM
+   Name Server: NS2.MEGACORPONE.COM
+   Name Server: NS3.MEGACORPONE.COM
+   DNSSEC: unsigned
+   URL of the ICANN Whois Inaccuracy Complaint Form: https://www.icann.org/wicf/
+>>> Last update of whois database: 2023-11-06T10:10:03Z <<<
+
+For more information on Whois status codes, please visit https://icann.org/epp
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the expiration
+date of the domain name registrant's agreement with the sponsoring
+registrar.  Users may consult the sponsoring registrar's Whois database to
+view the registrar's reported date of expiration for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign Global Registry
+Services' ("VeriSign") Whois database is provided by VeriSign for
+information purposes only, and to assist persons in obtaining information
+about or related to a domain name registration record. VeriSign does not
+guarantee its accuracy. By submitting a Whois query, you agree to abide
+by the following terms of use: You agree that you may use this Data only
+for lawful purposes and that under no circumstances will you use this Data
+to: (1) allow, enable, or otherwise support the transmission of mass
+unsolicited, commercial advertising or solicitations via e-mail, telephone,
+or facsimile; or (2) enable high volume, automated, electronic processes
+that apply to VeriSign (or its computer systems). The compilation,
+repackaging, dissemination or other use of this Data is expressly
+prohibited without the prior written consent of VeriSign. You agree not to
+use electronic processes that are automated and high-volume to access or
+query the Whois database except as reasonably necessary to register
+domain names or modify existing registrations. VeriSign reserves the right
+to restrict your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .COM, .NET, .EDU domains and
+Registrars.
+Domain Name: megacorpone.com
+Registry Domain ID: 1775445745_DOMAIN_COM-VRSN
+Registrar WHOIS Server: whois.gandi.net
+Registrar URL: http://www.gandi.net
+Updated Date: 2023-10-24T21:05:16Z
+Creation Date: 2013-01-22T22:01:00Z
+Registrar Registration Expiration Date: 2024-01-22T23:01:00Z
+Registrar: GANDI SAS
+Registrar IANA ID: 81
+Registrar Abuse Contact Email: abuse@support.gandi.net
+Registrar Abuse Contact Phone: +33.170377661
+Reseller: 
+Domain Status: clientTransferProhibited http://www.icann.org/epp#clientTransferProhibited
+Domain Status: 
+Domain Status: 
+Domain Status: 
+Domain Status: 
+Registry Registrant ID: 
+Registrant Name: Alan Grofield
+Registrant Organization: MegaCorpOne
+Registrant Street: 2 Old Mill St
+Registrant City: Rachel
+Registrant State/Province: Nevada
+Registrant Postal Code: 89001
+Registrant Country: US
+Registrant Phone: +1.9038836342
+Registrant Phone Ext:
+Registrant Fax: 
+Registrant Fax Ext:
+Registrant Email: 3310f82fb4a8f79ee9a6bfe8d672d87e-1696395@contact.gandi.net
+Registry Admin ID: 
+Admin Name: Alan Grofield
+Admin Organization: MegaCorpOne
+Admin Street: 2 Old Mill St
+Admin City: Rachel
+Admin State/Province: Nevada
+Admin Postal Code: 89001
+Admin Country: US
+Admin Phone: +1.9038836342
+Admin Phone Ext:
+Admin Fax: 
+Admin Fax Ext:
+Admin Email: 3310f82fb4a8f79ee9a6bfe8d672d87e-1696395@contact.gandi.net
+Registry Tech ID: 
+Tech Name: Alan Grofield
+Tech Organization: MegaCorpOne
+Tech Street: 2 Old Mill St
+Tech City: Rachel
+Tech State/Province: Nevada
+Tech Postal Code: 89001
+Tech Country: US
+Tech Phone: +1.9038836342
+Tech Phone Ext:
+Tech Fax: 
+Tech Fax Ext:
+Tech Email: 3310f82fb4a8f79ee9a6bfe8d672d87e-1696395@contact.gandi.net
+Name Server: NS1.MEGACORPONE.COM
+Name Server: NS2.MEGACORPONE.COM
+Name Server: NS3.MEGACORPONE.COM
+Name Server: 
+Name Server: 
+Name Server: 
+Name Server: 
+Name Server: 
+Name Server: 
+Name Server: 
+DNSSEC: Unsigned
+URL of the ICANN WHOIS Data Problem Reporting System: http://wdprs.internic.net/
+>>> Last update of WHOIS database: 2023-11-06T10:10:09Z <<<
+
+For more information on Whois status codes, please visit
+https://www.icann.org/epp
+
+Reseller Email: 
+Reseller URL: 
+
+Personal data access and use are governed by French law, any use for the purpose of unsolicited mass commercial advertising as well as any mass or automated inquiries (for any intent other than the registration or modification of a domain name) are strictly forbidden. Copy of whole or part of our database without Gandi's endorsement is strictly forbidden.
+A dispute over the ownership of a domain name may be subject to the alternate procedure established by the Registry in question or brought before the courts.
+For additional information, please contact us via the following form:
+ https://www.gandi.net/support/contacter/mail/
