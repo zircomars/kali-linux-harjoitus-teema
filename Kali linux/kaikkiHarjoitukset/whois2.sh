1e35c96dc7829bef5910cb500fb3f486728b19f8
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-06 12:28:46 +0200

    Create whois2.sh

---
+++ Kali linux/kaikkiHarjoitukset/whois2.sh
@@ -0,0 +1,59 @@
+┌──(kali㉿kali)-[~]
+└─$ host iltalehti.fi
+iltalehti.fi has address 18.66.147.86
+iltalehti.fi has address 18.66.147.66
+iltalehti.fi has address 18.66.147.48
+iltalehti.fi has address 18.66.147.127
+iltalehti.fi has IPv6 address 2600:9000:225e:5a00:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:ca00:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:a00:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:6600:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:a800:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:1400:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:ce00:9:1558:8f00:93a1
+iltalehti.fi has IPv6 address 2600:9000:225e:8a00:9:1558:8f00:93a1
+iltalehti.fi mail is handled by 10 iltalehti-fi.mail.protection.outlook.com.
+                                                                                                                       
+┌──(kali㉿kali)-[~]
+└─$ whois iltalehti.fi 
+
+domain.............: iltalehti.fi
+status.............: Registered
+created............: 1.1.1991 00:00:00
+expires............: 31.8.2024 00:00:00
+available..........: 30.9.2024 00:00:00
+modified...........: 27.7.2023 05:13:06
+holder transfer....: 13.7.2013 01:42:53
+RegistryLock.......: no
+
+Nameservers
+
+nserver............: ns-1702.awsdns-20.co.uk [OK]
+nserver............: ns-780.awsdns-33.net [OK]
+nserver............: ns-1529.awsdns-63.org [OK]
+nserver............: ns-488.awsdns-61.com [OK]
+
+DNSSEC
+
+dnssec.............: no
+
+Holder
+
+name...............: Alma Media Oyj
+register number....: 1944757-4
+address............: PL 140
+postal.............: 00101
+city...............: Helsinki
+country............: Finland
+phone..............: 
+holder email.......: 
+
+Registrar
+
+registrar..........: Gandi SAS
+www................: www.gandi.net
+
+>>> Last update of WHOIS database: 6.11.2023 12:15:05 (EET) <<<
+
+
+Copyright (c) Finnish Transport and Communications Agency Traficom
