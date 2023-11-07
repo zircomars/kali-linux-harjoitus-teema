f055cb1aff2f8ce2f0b8612393f424c93c8c07b8
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-07 15:48:41 +0200

    Update dns_server1.sh

--- Kali linux/kaikkiHarjoitukset/dns_server1.sh
+++ Kali linux/kaikkiHarjoitukset/dns_server1.sh
@@ -10,6 +10,7 @@
 
 # Oletusarvoisesti jokaisessa määritetyssä toimialueessa on oltava vähintään toimialueesta vastaavat DNS- ja sähköpostipalvelimet.
 
+########################################################################
 ┌──(kali㉿kali)-[~]
 └─$ host -t ns megacorpone.com
 megacorpone.com name server ns3.megacorpone.com.
@@ -26,11 +27,77 @@ megacorpone.com mail is handled by 50 mail.megacorpone.com.
 megacorpone.com mail is handled by 20 spool.mail.gandi.net.
 megacorpone.com mail is handled by 10 fb.mail.gandi.net.
 
+########################################################################
 # automating Lookups
 
+# kun on tiedossa jokin alustava tieto vaikappa megacorpone.com - domain (verkkotunnus) nimi, niin voidaan jatkaa DNS - requestiä (lisäkyselyä) käyttöön löydetäkseen sen tämän sivuston megacorpone - domain tunnus kuuluviin host:in nimi ja IP-osoite. 
+
+# periaatteessa: For example, we can assume that the megacorpone.com domain has a web server, probably with the hostname www. We can test this theory using the host command once again:
+
+┌──(kali㉿kali)-[~]
+└─$ host www.megacorpone.com  
+www.megacorpone.com has address 149.56.244.87
+                                                                                                                                                            
+##########
+# seuraavaksi, jos tarkistellaan onko tällä domain (megacorpone.com) sillä on myös palvelin, jonka isäntänimi on idontexist.
+# Huomaa ero kyselytulosteiden välillä
+
+┌──(kali㉿kali)-[~]
+└─$ host idontexist.megacorpone.com
+Host idontexist.megacorpone.com not found: 3(NXDOMAIN)
 
 
+########################################################################
+# forward lookup brute force
 
+# edellisessä harjoituksessa eli (automating lookups), ja nyt tähän lisätään yleisiä host nimejä, johon suoritettaan Bash scriptaus. 
+
+# Tämän tekniikan ideana on arvata palvelimien kelvollisen nimet yrittämällä ratkaista annettu nimi. Jos arvaamasi nimi ratkaisee, tulokset saattavat viitata palvelimen olemassaoloon ja jopa toimivuuteen. Voimme luoda lyhyen (tai pitkän) luettelon mahdollisista isäntänimistä ja kokeilla kutakin host:in komentoa.
+
+# ensimmäisenä toistettaan "www" tonne list.txt sisään , ja sitten sama noi muutkin tulee 
+──(kali㉿kali)-[~]
+└─$ echo www > list.txt
+                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ echo ftp >> list.txt 
+                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ echo mail >> list.txt 
+                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ echo owa >> list.txt 
+                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ echo proxy >> list.txt 
+                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ echo router >> list.txt 
+
+# jos tarkistettaan list.txt                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ cat list.txt
+www
+ftp
+mail
+owa
+proxy
+router
+
+
+#### step 2
+# In this simplified example, we notice that the hostnames www, router, and mail have been discovered through this brute-force attack. The hostnames owa, ftp and proxy, however, were not found.
+
+# tässä periaatteessa for looppin kautta, että käytettän list.txt sisäisiä objektia, että tarkistellaan tämän domain.com (megacorpone.com) verkkotunnus prosessi
+# eli tässä tarkistettiin host nimi, www, routeri ja sähköposti, jotka löydettiin näiden raa'an voiman hyökkäyksen avulla, joten host nimet owa, ftp ja proxy, niistä ei löytynyt mitään tietoa
+┌──(kali㉿kali)-[~]
+└─$ for ip in $(cat list.txt);do host $ip.megacorpone.com;done
+www.megacorpone.com has address 149.56.244.87
+;; communications error to 192.168.153.2#53: timed out
+Host ftp.megacorpone.com not found: 3(NXDOMAIN)
+mail.megacorpone.com has address 51.222.169.212
+Host owa.megacorpone.com not found: 3(NXDOMAIN)
+Host proxy.megacorpone.com not found: 3(NXDOMAIN)
+router.megacorpone.com has address 51.222.169.214
 
 
 
