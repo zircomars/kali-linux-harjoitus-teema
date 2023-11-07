7916d85c7d6c91d399c2cd797b637ad1b3ed8a4a
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-07 14:56:28 +0200

    Update scan_domain1.sh

--- Kali linux/nikto/harjoitukset/scan_domain1.sh
+++ Kali linux/nikto/harjoitukset/scan_domain1.sh
@@ -19,9 +19,28 @@
 + /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
 + /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
 ^C                                                                                                                       
-┌──(kali㉿kali)-[~]
-└─$ 
 
+
+# toinen esim.
+──(kali㉿kali)-[~]
+└─$ nikto -h http://testhtml5.vulnweb.com
+- Nikto v2.5.0
+---------------------------------------------------------------------------
++ Target IP:          44.228.249.3
++ Target Hostname:    testhtml5.vulnweb.com
++ Target Port:        80
++ Start Time:         2023-11-07 14:52:30 (GMT2)
+---------------------------------------------------------------------------
++ Server: nginx/1.19.0
++ /: Retrieved access-control-allow-origin header: *.
++ /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
++ /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
++ ERROR: Error limit (20) reached for host, giving up. Last error: error reading HTTP response
++ Scan terminated: 20 error(s) and 3 item(s) reported on remote host
++ End Time:           2023-11-07 14:54:12 (GMT2) (102 seconds)
+---------------------------------------------------------------------------
++ 1 host(s) tested
+                  
 
 
 
