8b58604936875a61691705e1442b4cc1c32745d3
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-07 13:52:38 +0200

    Create begin1.sh

---
+++ Kali linux/nikto/harjoitukset/begin1.sh
@@ -0,0 +1,25 @@
+# harjoitus linkki;; https://www.freecodecamp.org/news/an-introduction-to-web-server-scanning-with-nikto/
+
+
+# ensimmäisessä skannauksessa tapahtuu Nikto suorittaa perusskannauksen portissa 80 tietylle toimialueelle ja antaa täydellisen raportin suoritettujen tarkistusten perusteella ja siksi peruuttin eli CTRL + C
+
+┌──(kali㉿kali)-[~]
+└─$ nikto -h scanme.nmap.org
+- Nikto v2.5.0
+---------------------------------------------------------------------------
++ Multiple IPs found: 45.33.32.156, 2600:3c01::f03c:91ff:fe18:bb2f
++ Target IP:          45.33.32.156
++ Target Hostname:    scanme.nmap.org
++ Target Port:        80
++ Start Time:         2023-11-07 13:41:31 (GMT2)
+---------------------------------------------------------------------------
++ Server: Apache/2.4.7 (Ubuntu)
++ /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
++ /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
++ No CGI Directories found (use '-C all' to force check all possible dirs)
++ Apache/2.4.7 appears to be outdated (current is at least Apache/2.4.54). Apache 2.2.34 is the EOL for the 2.x branch.
++ /index: Uncommon header 'tcn' found, with contents: list.
++ /index: Apache mod_negotiation is enabled with MultiViews, which allows attackers to easily brute force file names. The following alternatives for 'index' were found: index.html. See: http://www.wisec.it/sectou.php?id=4698ebdc59d15,https://exchange.xforce.ibmcloud.com/vulnerabilities/8275
++ OPTIONS: Allowed HTTP Methods: GET, HEAD, POST, OPTIONS .
+^
+┌──(kali㉿kali)-[~]
