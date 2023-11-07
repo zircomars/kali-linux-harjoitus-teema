c8b453292f4cf3cbc0a8c6e45458e0a509cd5d36
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-07 15:01:45 +0200

    Update export_scanResult1.sh

--- Kali linux/nikto/harjoitukset/export_scanResult1.sh
+++ Kali linux/nikto/harjoitukset/export_scanResult1.sh
@@ -37,3 +37,52 @@ drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Videos
 └─$ cat scan.txt 
 - Nikto v2.5.0/
 
+
+###########################################################
+
+# toinen esim. mutta käytetään toisella verko sivustolla
+
+┌──(kali㉿kali)-[~]
+└─$ nikto -h http://testhtml5.vulnweb.com -o scanweb.txt
+- Nikto v2.5.0
+---------------------------------------------------------------------------
++ Target IP:          44.228.249.3
++ Target Hostname:    testhtml5.vulnweb.com
++ Target Port:        80
++ Start Time:         2023-11-07 14:57:31 (GMT2)
+---------------------------------------------------------------------------
++ Server: nginx/1.19.0
++ /: Retrieved access-control-allow-origin header: *.
++ /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
++ /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
++ ERROR: Error limit (20) reached for host, giving up. Last error: error reading HTTP response
++ Scan terminated: 19 error(s) and 3 item(s) reported on remote host
++ End Time:           2023-11-07 14:58:56 (GMT2) (85 seconds)
+---------------------------------------------------------------------------
++ 1 host(s) tested
+ 
+# se on siellä ja joko komennolla $ll tai $ls (eli lista)
+┌──(kali㉿kali)-[~]
+└─$ ll    
+total 36
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Desktop
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Documents
+drwxr-xr-x 2 kali kali 4096 Oct 31 11:13 Downloads
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Music
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Pictures
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Public
+-rw-r--r-- 1 kali kali  547 Nov  7 14:58 scanweb.txt
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Templates
+drwxr-xr-x 2 kali kali 4096 Oct 31 10:59 Videos
+                                                                                                                                                            
+┌──(kali㉿kali)-[~]
+└─$ cat scanweb.txt 
+- Nikto v2.5.0/
++ Target Host: testhtml5.vulnweb.com
++ Target Port: 80
++ GET /: Retrieved access-control-allow-origin header: *.
++ GET /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options: 
++ GET /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/: 
+                                                                                                                                                            
+
+
