c07a4941f978875eba602bf736de1c140bf360a4
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-08 20:40:01 +0200

    Update dns_server1.sh

--- Kali linux/kaikkiHarjoitukset/dns_server1.sh
+++ Kali linux/kaikkiHarjoitukset/dns_server1.sh
@@ -1,5 +1,4 @@
 # avoimien verkkojen resurssien ja muita passiivisien tiedon keruu tekniikkaa, jotta voidaan kerätä lisää oleellisia tietoja muista, tarkemmista palveluista.
-
 # DNS on usein tuottoisin lähde (source) aktiivisiin tiedonkeruulle. DNS periaatteessa tarjoaa erilaisia tietoja julkisia ja miks ei yksityisissä, jossa organisaation palvelimista kuten on IP-osoite, palvelin serverin nimi ja palvelin funktionaali (toimintoja)
 
 # DNS palvelin yleensä paljastavat DNS- ja sähköpostipalvelimien tietoja verkkotunnuksista (domain), johon sillä on valtanssa. Tämä on välttämätöntä, koska julkiset sähköposti- ja DNS-palvelinosoitteiden pyynnöt muodostavat Internetin peruskokemuksen. 
@@ -9,6 +8,13 @@
 # Käytämme isäntäkomentoa yhdessä parametrin –t (type) kanssa löytääksemme sekä DNS- että sähköpostipalvelimet megacorpone.com-verkkotunnukselle
 
 # Oletusarvoisesti jokaisessa määritetyssä toimialueessa on oltava vähintään toimialueesta vastaavat DNS- ja sähköpostipalvelimet.
+
+#############
+# harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
+# chapter 4. active information gathering
+# sivu start here: 98-
+
+# samat harjoitukset ja scriptit: https://github.com/muckitymuck/OSCP-Study-Guide/blob/master/enumeration/active_information_gathering.md 
 
 ########################################################################
 ┌──(kali㉿kali)-[~]
@@ -206,6 +212,10 @@ ns3.megacorpone.com.
 ns1.megacorpone.com.
 ns2.megacorpone.com.
 
+######## valinnainen , oma harjotius ########
+# lisäharjoituksena, tätä voisi jopa kokeilla muita domain nimejä (verkkotunnuksia domain.fi), ja tarkistaa mitä zone transferiä se tulostaa ja jne.
+# ja jos saisi niiden IP-osoitteet ja siitä metsätää niiden dns-nimi palvelimet
+
 ###### bash script malli
 # jos ei muista miten luotiin joku tiedosto niin...
 ┌──(kali㉿kali)-[~]
@@ -256,13 +266,68 @@ www.megacorpone.com has address 149.56.244.87
 www2.megacorpone.com has address 149.56.244.87
 
 
-######## valinnainen ########
-# lisäharjoituksena, tätä voisi jopa kokeilla muita domain nimejä (verkkotunnuksia domain.fi), ja tarkistaa mitä zone transferiä se tulostaa ja jne.
-# ja jos saisi niiden IP-osoitteet ja siitä metsätää niiden dns-nimi palvelimet
+##################################
+### DNSRecon
+# DNSRecon on edistyksellinen, moderni Pythonilla kirjoitettu DNS luettelo scripti. DNSrecord komennon scriptin suorittamisen "megacorpone.com" domain vastaan tuottaa seuraava tulokset:
 
-#########
+┌──(kali㉿kali)-[~]
+└─$  dnsrecon -d megacorpone.com -t axfr
+[*] Checking for Zone Transfer for megacorpone.com name servers
+[*] Resolving SOA Record
+[+]      SOA ns1.megacorpone.com 51.79.37.18
+[*] Resolving NS Records
+[*] NS Servers found:
+[+]      NS ns3.megacorpone.com 66.70.207.180
+[+]      NS ns2.megacorpone.com 51.222.39.63
+[+]      NS ns1.megacorpone.com 51.79.37.18
+[*] Removing any duplicate NS server IP Addresses...
+[*]  
+[*] Trying NS server 51.222.39.63
+[+] 51.222.39.63 Has port 53 TCP Open
+[+] Zone Transfer was successful!!
+[*]      NS ns1.megacorpone.com 51.79.37.18
+[*]      NS ns2.megacorpone.com 51.222.39.63
+[*]      NS ns3.megacorpone.com 66.70.207.180
+[*]      TXT Try Harder
+[*]      TXT google-site-verification=U7B_b0HNeBtY4qYGQZNsEYXfCJ32hMNV3GtC0wWq5pA
+[*]      MX @.megacorpone.com fb.mail.gandi.net 217.70.178.217
+[*]      MX @.megacorpone.com fb.mail.gandi.net 217.70.178.215
+[*]      MX @.megacorpone.com fb.mail.gandi.net 217.70.178.216
+[*]      MX @.megacorpone.com fb.mail.gandi.net 2001:4b98:dc4:8::215
+[*]      MX @.megacorpone.com fb.mail.gandi.net 2001:4b98:dc4:8::216
+[*]      MX @.megacorpone.com fb.mail.gandi.net 2001:4b98:dc4:8::217
+[*]      MX @.megacorpone.com spool.mail.gandi.net 217.70.178.1
+[*]      MX @.megacorpone.com spool.mail.gandi.net 2001:4b98:e00::1
+[*]      A admin.megacorpone.com 51.222.169.208
+[*]      A beta.megacorpone.com 51.222.169.209
+[*]      A fs1.megacorpone.com 51.222.169.210
+[*]      A intranet.megacorpone.com 51.222.169.211
+[*]      A mail.megacorpone.com 51.222.169.212
+[*]      A mail2.megacorpone.com 51.222.169.213
+[*]      A ns1.megacorpone.com 51.79.37.18
+[*]      A ns2.megacorpone.com 51.222.39.63
+[*]      A ns3.megacorpone.com 66.70.207.180
+[*]      A router.megacorpone.com 51.222.169.214
+[*]      A siem.megacorpone.com 51.222.169.215
+[*]      A snmp.megacorpone.com 51.222.169.216
+[*]      A support.megacorpone.com 51.222.169.218
+[*]      A syslog.megacorpone.com 51.222.169.217
+[*]      A test.megacorpone.com 51.222.169.219
+[*]      A vpn.megacorpone.com 51.222.169.220
+[*]      A www.megacorpone.com 149.56.244.87
+[*]      A www2.megacorpone.com 149.56.244.87
+[*]  
+[*] Trying NS server 66.70.207.180
+[+] 66.70.207.180 Has port 53 TCP Open
+[-] Zone Transfer Failed (Zone transfer error: REFUSED)
+[*]  
+[*] Trying NS server 51.79.37.18
+[+] 51.79.37.18 Has port 53 TCP Open
+[-] Zone Transfer Failed (Zone transfer error: REFUSED)
 
-
+######## valinnainen , oma harjotius ########
+# lisäharjoituksena, tätä voisi jopa kokeilla muita domain nimejä (verkkotunnuksia domain.fi), ja tarkistaa mitä se tulostaa mm. dns palvelin, ip-sooite, zone transfer, mac osoite ja jne.
+# sekä mitä yksityiskohtaisia tietoja se saattaa tulostaa ja hertaja
 
 
 
