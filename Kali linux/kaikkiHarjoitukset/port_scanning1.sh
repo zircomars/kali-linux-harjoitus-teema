e89d36f1afd617656545961f6e888315e8bebfa2
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-10 19:08:51 +0200

    Update port_scanning1.sh

--- Kali linux/kaikkiHarjoitukset/port_scanning1.sh
+++ Kali linux/kaikkiHarjoitukset/port_scanning1.sh
@@ -1,7 +1,7 @@
 #############
 # harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
 # chapter 4. active information gathering
-# sivu start here: 109 - 
+# sivu start here: 109 - 117
 
 # samat harjoitukset ja scriptit: https://github.com/muckitymuck/OSCP-Study-Guide/blob/master/enumeration/active_information_gathering.md 
 
