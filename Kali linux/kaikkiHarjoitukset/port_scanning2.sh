3eb61f306c749a98efa18195c75869860e0055d9
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-10 19:09:30 +0200

    Create port_scanning2.sh

---
+++ Kali linux/kaikkiHarjoitukset/port_scanning2.sh
@@ -0,0 +1,6 @@
+#############
+# harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
+# chapter 4. active information gathering
+# sivu START HERE: 118 - 
+
+# samat harjoitukset ja scriptit: https://github.com/muckitymuck/OSCP-Study-Guide/blob/master/enumeration/active_information_gathering.md 
