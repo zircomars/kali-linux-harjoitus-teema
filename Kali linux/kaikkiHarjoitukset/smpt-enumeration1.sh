2660b1b144dffc6d0edd60b28375c359e23f68ae
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-11 20:44:07 +0200

    Update smpt-enumeration1.sh

--- Kali linux/kaikkiHarjoitukset/smpt-enumeration1.sh
+++ Kali linux/kaikkiHarjoitukset/smpt-enumeration1.sh
@@ -15,8 +15,12 @@
   # - EXPN kysyy palvelimelta postituslistan jäsenyyttää (membership of a mailing list)
 
 # näitä voidaaan usein käyttää vääriin todentamisiin olemassa oleviin käyttäjiä sähköpostipalvelimella, mikä voi myöhemin auttaa hyökkääjää.
+# SMPT oletus portti on 25, se on TCP protokolla. Jotkut verkkovalvojat (network admin) saattavat päättää määrittää toisen portin SMTP palvelun suorittamista.
+# Jos SMTP palvelussa on virheitä tai palvelimessa on haavoittuvuus, siinä tapauksessa voidaan saavutta useita asioita mm:
+  # - Voimme luetella ja kerätä käyttäjätilejä
+  # - Voimme ohittaa todennuksen ja lähettää sähköposteja, jos meillä on avoin välitys.
 
-# esim. komento scripti
+# esim. komento scripti & nc = netcat
 root@kali:~# nc -nv 10.11.1.215 25
 (UNKNOWN) [10.11.1.215] 25 (smtp) open
 220 redhat.acme.com ESMTP Sendmail 8.12.8/8.12.8; Wed, 12 Jun 2013 07:47:14 +0300
@@ -27,7 +31,8 @@ VRFY idontexist
 ^C
 root@kali:~# 
 
-
+# This procedure can be used to help guess valid usernames.
+> nc -nv 192.168.11.215 25
 
 
 
