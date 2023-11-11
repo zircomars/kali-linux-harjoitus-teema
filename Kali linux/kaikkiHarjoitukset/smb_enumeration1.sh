21075d2c6fb330559fe82c954bd3d20be5e347b9
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-11 18:28:40 +0200

    Update smb_enumeration1.sh

--- Kali linux/kaikkiHarjoitukset/smb_enumeration1.sh
+++ Kali linux/kaikkiHarjoitukset/smb_enumeration1.sh
@@ -1,7 +1,7 @@
 #############
 # harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
 # chapter 4. active information gathering
-# sivu START HERE: 122
+# sivu START HERE: 122 - 129
 
 # samat harjoitukset ja scriptit: https://github.com/muckitymuck/OSCP-Study-Guide/blob/master/enumeration/active_information_gathering.md 
 
