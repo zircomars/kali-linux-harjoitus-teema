21d809a39a1b0b552498dda3b60be0a18316dc88
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-11 16:06:48 +0200

    Create smb_enumeration1.sh

---
+++ Kali linux/kaikkiHarjoitukset/smb_enumeration1.sh
@@ -0,0 +1,74 @@
+#############
+# harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
+# chapter 4. active information gathering
+# sivu START HERE: 122
+
+# samat harjoitukset ja scriptit: https://github.com/muckitymuck/OSCP-Study-Guide/blob/master/enumeration/active_information_gathering.md 
+
+########################################################################################
+##### The server message block (SMB) protokollan turvallisuudessa muinoin historiassa on ollut heikko prosessi n. yli kymmenen vuoden ajalta ja sen monimutkaisen toteutuksen ja avoimien luonteen vuooksi (open nature)
+
+# todentamisessa (unauthenticated) SMB-nolla istunnoissa Windows 2000 ja XP:ssä lukuisiin SMB-virheitä (buggia) ja haavoittuvuuksia vuosien varrella, ja SMB on nähnyt useita kohtuulisia osuutensa toimintoja
+
+# SMB protokolla on kuitenkin myös päivitetty versio ja parantanut samanaikaisesti Windows- käyttöjärjestelmien julkaisujen kanssa ja muutamista luettelosta SMB-versioidenn ja Windows käyttöjärjestelminen versioiden selventämistä
+
+# o SMB1 – Windows 2000, XP and Windows 2003.
+# o SMB2 – Windows Vista SP1 and Windows 2008
+# o SMB2.1 – Windows 7 and Windows 2008 R2
+# o SMB3 – Windows 8 and Windows 2012
+
+############################
+#### Scanning for the NetBIOS Service ######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
