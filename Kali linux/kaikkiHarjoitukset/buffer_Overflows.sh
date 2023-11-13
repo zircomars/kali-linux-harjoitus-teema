719925841ffd1a139c2ccfce582bd9b61c1a5002
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-13 13:34:06 +0200

    Create buffer_Overflows.sh

---
+++ Kali linux/kaikkiHarjoitukset/buffer_Overflows.sh
@@ -0,0 +1,26 @@
+#############
+# harjoitus; https://edu.anarcho-copy.org/Against%20Security%20-%20Self%20Security/Offensive%20Security%20-%20Pentesting%20with%20Kali%20(PWK).pdf
+# chapter 6. - Buffer Overflows
+# sivu START HERE: 148 - 
+
+#######################################################################################################
+####### Buffer Overflows ##########
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
