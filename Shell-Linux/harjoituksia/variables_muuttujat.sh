c527341495bcded0f88e0bbbf4aad2411c213544
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-26 16:45:09 +0200

    Update variables_muuttujat.sh

--- Shell-Linux/harjoituksia/variables_muuttujat.sh
+++ Shell-Linux/harjoituksia/variables_muuttujat.sh
@@ -1,4 +1,5 @@
 # muuttujan (variable) arvo muuttaminen tai antaminen, tämä toimii periaatteessa yksi suora arvoisena, että pitää antaa "nimi=arvo" jotakin ja ilmoittamisessa edessä ja takana ei saa olla välilyöntiä, jos antaa niin se tositaa jokin pien error:in
+# tätä voi periaatteessa hyödyntää mm. cmd/powershell ja linux / kali linux istunnossa
 
 ~/variables-muuttujat$ first_name=Mikael
 ~/variables-muuttujat$ last_name=Doe_Hacker
@@ -14,3 +15,58 @@ Mikael Doe_Hacker
 harjoitus2.txt  list.txt  main.sh  muistio.txt
 ~/variables-muuttujat$ cat list.txt 
 Mikael Doe_Hacker
+
+# dollari merki edessä tarkoittaa ikään kuin viitattaan siihen tekijäänsä esim. a = arvo jotakin, mutta dollarimerkki pitää olla sen edessä kun yritettää ntoistaa jotakin vastaavaa
+
+# muuttujista voi olla iso, pien, ja erikois merkkiä tai yhdistelmiä. bash sallii isoja ja pien kirjaimia, johon voidaan oltava johdonmukainen muuttuja ilmoittamisessa ja laajentamisessa.
+
+# lisäksi muuttujien nimessä, jotta tehdään scriptiä on paljo helpompi lukea ja ylläpitää
+
+
+# lisäksi jos on jokin lause vaikappa tervehdys tai vastaava;
+#esim. tämä pieni error
+~/variables-muuttujat$ greeting=hello world
+bash: world: command not found
+
+# jos haluttaan korjata, että toistetaan jokin lause tai vastaava niin pitää lisätä tommoiset heittomerkkit kaksi kertaa "" tai '' koska että on alku ja päättyvät lainausmerkki
+# kun on suljettu heittomerkki " ja kaikkiin merkit katsotaan kirjaimellisesti eli dollari merkkinä $ ja \ (kautta viiva) mikä tarkoittaa, että muuttujat laajennettaan lakuperäisestä korvaavasta tekstistä
+
+~/variables-muuttujat$ greeting='moikka kuis hurisee?'
+~/variables-muuttujat$ echo $greeting 
+moikka kuis hurisee?
+~/variables-muuttujat$ 
+
+~/variables-muuttujat$  greeting2="New $greeting"
+~/variables-muuttujat$ echo $greeting
+moikka kuis hurisee?
+~/variables-muuttujat$ 
+
+~/variables-muuttujat$ greeting="joulu on taas"
+~/variables-muuttujat$ echo $greeting
+joulu on taas
+
+## sitten muita esimerkkejä muuttujista
+
+kali@kali:~$ user=$(whoami)
+kali@kali:~$ echo $user
+kali
+
+# tiedoston tai jotakin scriptien toistamista, esim. alemmassa ./main.sh jossa toistettaan tai suoritettaan scriptin ominaisuus
+~/variables-muuttujat$ cat ./list.txt 
+Mikael Doe_Hacker
+
+~/variables-muuttujat$ ./main.sh 
+File Name: ./main.sh
+First Parameter : 
+Toinen Parameter : 
+Quoted Values: 
+Quoted Values: 
+asdf: 0
+Software testing: 16592
+zxcv: 
+Total Number of Parameters : 0
+
+
+
+
+
