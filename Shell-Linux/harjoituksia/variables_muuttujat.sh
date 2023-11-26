ce91bf0f23e64bd8334699fb82ac35232a6f9be6
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-26 14:38:55 +0200

    Create variables_muuttujat.sh

---
+++ Shell-Linux/harjoituksia/variables_muuttujat.sh
@@ -0,0 +1,8 @@
+~/variables-muuttujat$ first_name=Mikael
+~/variables-muuttujat$ last_name=Doe_Hacker
+~/variables-muuttujat$ last_name=Doe_Hacker
+~/variables-muuttujat$ echo $first_name $last_name 
+Mikael Doe_Hacker
+
+
+# ei salli välilyöntiä/space
