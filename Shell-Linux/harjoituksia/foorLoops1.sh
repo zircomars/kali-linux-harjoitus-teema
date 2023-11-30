9612d1ef0d76cbdda90a06e6274346ce462c5fad
zircomars <61022230+zircomars@users.noreply.github.com>
2023-11-30 20:16:45 +0200

    Update foorLoops1.sh

--- Shell-Linux/harjoituksia/foorLoops1.sh
+++ Shell-Linux/harjoituksia/foorLoops1.sh
@@ -8,8 +8,13 @@ do
  <action to perform>
 done
 
-esim. jos shellissä suoritettaan jotakin pinggausta, ip-osoitteeseen nin se tekee sen 10.11.1.1 - 10.11.1.10 asti
-siksi siinä on alkaminen 10.11.1.X jotakin
+# tässä pari ensimmäistä esimerkkiä on paljon potentiaalia. IP-osoitteiden for loop jotakin IP-osoitteiden näyttämisessä ei hyödynnä mitään kauheasti, mutta sitä voi käyttää samaa ideaa porttienskannauksessa suorittamiseen nmap prosesisissa. 
+# Tätä voidaan periaattessa yrittää käyttää pinggien komentoa ja vastaako jokin IP-osoitteesta ICMP echo pyyntöön.
+
+# ICMP = Internet Control Message Protocol
+
+#esim. jos shellissä suoritettaan jotakin pinggausta, ip-osoitteeseen nin se tekee sen 10.11.1.1 - 10.11.1.10 asti
+#siksi siinä on alkaminen 10.11.1.X jotakin
 $for ip in $(seq 1 10); do echo 10.11.1.$ip; done
 10.11.1.1
 10.11.1.2
@@ -34,3 +39,28 @@ kali@kali:~$ for i in {1..10}; do echo 10.11.1.$i;done
 10.11.1.8
 10.11.1.9
 10.11.1.10
+
+#############################################
+
+# while loops
+
+while [ <some test> ]
+do
+ <perform an action>
+done
+
+#esim. scriptti
+#!/bin/bash
+# while loop example
+counter=1
+while [ $counter -lt 10 ]
+do
+ echo "10.11.1.$counter"
+ ((counter++))
+done
+
+
+
+
+
+
