cfc7242cd9db20b9d9312979e2a23438549c4de3
zircomars <61022230+zircomars@users.noreply.github.com>
2023-12-04 19:52:12 +0200

    Create wget_harj3.sh

---
+++ Kali linux/kaikkiHarjoitukset/wget_harj3.sh
@@ -0,0 +1,15 @@
+# Offensive-Security-OSCP-by-Offensive-Security_2020
+# https://blactec.biz/wp-content/uploads/2021/09/Offensive-Security-OSCP-by-Offensive-Security%E2%AD%90%E2%AD%90%E2%AD%90%E2%AD%90%E2%AD%90%E2%AD%90%E2%AD%90%E2%AD%90.pdf
+
+#tämä on sama idea kuin toi vanhempi 2018 julkaistettu versio, mutta tässä harjoituksessa käytettään sen esimerkkin linkkiä "megacorpone.com" 
+#tätä voi periaatteessa suorittaa virallisesti omass kali linux:sessa, mutta testaan repl.it bash:in kautta
+
+# PAGE START 129 - 135 (EXAMPLE 3)
+
+penetraatiotestauksessa yritettään usein löytää tehokkuutta minimoidakseen datan analysointia kuluvien ajan, josta erityisesti erilaisten skannausten aikana palutettavien datamäärää.
+
+
+
+
+
+
