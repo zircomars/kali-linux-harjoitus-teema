0edff3e42dd7aeb473064b3b8b67a2de4fd8dea3
zircomars <61022230+zircomars@users.noreply.github.com>
2023-12-09 13:39:41 +0200

    Update workplaces1.sh

--- Kali linux/recon-ng/harjoitukset/workplaces1.sh
+++ Kali linux/recon-ng/harjoitukset/workplaces1.sh
@@ -1,8 +1,33 @@
 # workplaces harjoitus, josta tapahtuu "create", "list", "load" ja "remove" workspace toimintoja 
 
+# luodaan joku workspace (vähä kuin sql komento)
+[recon-ng][default] > workspaces create datakanta
 
+# toistetaan ja tarkistellaan listat, ja oletuksena (default) tulee mukaan aina
+[recon-ng][datakanta] > workspaces list
 
+  +----------------------------------+
+  | Workspaces |       Modified      |
+  +----------------------------------+
+  | datakanta  | 2023-12-09 13:34:48 |
+  | default    | 2023-12-07 18:04:48 |
+  +----------------------------------+
 
+# workspace <jotakin> niin mennään takaisin polkuun (default) on aina mukana jos halutaan ikään kuin peruuttaa taaksepäin sitä polkua
+# ja sama kun on luotu "datakanta" niin load <nimi> niin sillä päästään sinne datan (kansion) sisälle
+[recon-ng][datakanta] > workspaces load default
+
+# poistaminen?
+# poistamiesssa missä polussa onkaan niin "remove" <jotakin> niin sillä kautta ja voi varmuuden vuodella tarkistaa onko kyseinen "kansio" poistunut listan alta
+[recon-ng][default] > workspaces load datakanta
+[recon-ng][datakanta] > workspaces remove datakanta
+[recon-ng][default] > workspaces list
+
+  +----------------------------------+
+  | Workspaces |       Modified      |
+  +----------------------------------+
+  | default    | 2023-12-07 18:04:48 |
+  +----------------------------------+
 
 
 
