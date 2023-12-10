c585c105baf7e6e5abdbabd2181a260e3e2acea6
zircomars <61022230+zircomars@users.noreply.github.com>
2023-12-10 17:00:25 +0200

    Update geo-location1.sh

--- Kali linux/recon-ng/harjoitukset/geo-location1.sh
+++ Kali linux/recon-ng/harjoitukset/geo-location1.sh
@@ -1,1 +1,24 @@
+# harjoitus (geo location)
+
+# ensimmäisenä tarkistellaan ja luodaan tyhjä "workspace" eli kuin sql joku tietokanta, ja siihen tarkistellaan. usein tyhjästä , jostakin on tyhjä "host" ja "domain"
+
+# jos ei ole luonut workspace (tietokantaa) tai jotakin tässä pieni lunttilappuna, ja samalla voi tarkistaa olemassa olevia workspace listoja.
+
+[recon-ng][default] > workspaces create ipgeo
+[recon-ng][ipgeo] > workspaces list
+
+  +----------------------------------+
+  | Workspaces |       Modified      |
+  +----------------------------------+
+  | default    | 2023-12-09 20:56:31 |
+  | ipgeo      | 2023-12-10 16:41:58 |
+  +----------------------------------+
+
+# ja voi varmuuden vuodella tarkistaa onko luoneen tieokannan "ipgeo":ssa mitään hostia ja/tai domainia (yleensä luoneen tyhjän workspace:ssä on tyhjää dataa)
+[recon-ng][ipgeo] > show hosts
+[*] No data returned.
+[recon-ng][ipgeo] > show domains
+[*] No data returned.
+
+
 
