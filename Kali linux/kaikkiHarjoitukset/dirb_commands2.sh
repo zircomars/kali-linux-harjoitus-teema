c839158f235311ed7b9bbbb1f05b2c566cd9b5e0
zircomars <61022230+zircomars@users.noreply.github.com>
2024-01-24 21:04:18 +0200

    Create dirb_commands2.sh

---
+++ Kali linux/kaikkiHarjoitukset/dirb_commands2.sh
@@ -0,0 +1,14 @@
+# DIRB 
+# special vulnerable list
+# https://medium.com/tech-zoom/dirb-a-web-content-scanner-bc9cba624c86
+
+
+
+
+┌──(kali㉿kali)-[/usr/share/dirb/wordlists/vulns]
+└─$ ls
+apache.txt      domino.txt             hpsmh.txt     jboss.txt    oracle.txt      sunas.txt     weblogic.txt
+axis.txt        fatwire_pagenames.txt  hyperion.txt  jersey.txt   ror.txt         tests.txt     websphere.txt
+cgis.txt        fatwire.txt            iis.txt       jrun.txt     sap.txt         tomcat.txt
+coldfusion.txt  frontpage.txt          iplanet.txt   netware.txt  sharepoint.txt  vignette.txt
+
