245d7e2c881698cf676c611c9fa22c170cb0829c
zircomars <61022230+zircomars@users.noreply.github.com>
2024-01-24 19:33:05 +0200

    Create WebContentScanner_DIRB1.sh

---
+++ Kali linux/kaikkiHarjoitukset/WebContentScanner1.sh
@@ -0,0 +1,4 @@
+<# DIRB is a Web Content Scanner. It looks for existing (and/or hidden) Web Objects. It basically works by launching a dictionary based attack against a web server and analyzing the responses.
+
+
+#>
